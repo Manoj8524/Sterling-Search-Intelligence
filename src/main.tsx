f7a560c4af7f9e0d3f4cb06750cd5e359e8281f8
Manoj M <manoj852408@gmail.com>
2026-09-07 10:43:49 +0530

    Use RouterProvider for client SPA rendering to fix invariant error

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { StartClient } from "@tanstack/react-start/client";
+import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
 import "./styles.css";
 
@@ -10,7 +10,7 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <StartClient router={router} />
+      <RouterProvider router={router} />
     </StrictMode>
   );
 }
